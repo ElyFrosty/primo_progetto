462533b834e83b7c243486ee9ed33efaf74ae4e5
ElyFrosty <elisa.freddi@alessandrinimainardi.edu.it>
2023-11-24 10:26:34 +0100

    passo 18

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Articolo,Giornalista
+import datetime
 
 # Create your views here.
 '''
@@ -43,3 +44,87 @@ def articoloDetailView(request,pk):
     articolo=get_object_or_404(Articolo, pk=pk)
     context={"articolo": articolo}
     return render(request, "articolo_detail.html", context)
+
+def listaArticoli(request,pk=None):
+    if (pk==None):
+        articoli=Articolo.objects.all()
+        titolo="Tutti gli articoli: "
+    else:
+        articoli=Articolo.objects.filter(giornalista_id=pk)
+        titolo="Articoli di un giornalista: "
+
+    context={
+        'articoli':articoli,
+        'titolo':titolo,
+    }
+    return render(request, 'lista_articoli.html', context)
+
+def queryBase(request):
+    #1. Tutti gli articoli scritti da giornalisti di un certo cognome:
+    articoli_cognome=Articolo.objects.filter(giornalista__cognome='Rossi')
+
+    #2. Totale:
+    numero_totale_articoli=Articolo.objects.count()
+
+    #3. Contare il numero di articoli scritti da un giornalista specifico:
+    giornalista_1=Giornalista.objects.get(id=2)
+    numero_articoli_giornalista_1=Articolo.objects.filter(giornalista=giornalista_1).count()
+
+    #4. Ordinare gli articoli per numero di visualizzazioni in ordine decrescente:
+    articoli_ordinati=Articolo.objects.order_by('-visualizzazzioni')
+
+    #5. Tutti gli articoli che non hanno visualizzazioni:
+    articoli_senza_visualizzazioni=Articolo.objects.filter(visualizzazzioni=0)
+
+    #6. Articolo più visualizzato:
+    articolo_piu_visualizzato=Articolo.objects.order_by('-visualizzazzioni').first()
+
+    #7. Tutti i giornalisti nati dopo una certa data:
+    giornalisti_data=Giornalista.objects.filter(anno_di_nascita__gt=datetime.date(1999,1,1))
+    
+    #8. Tutti gli articoli pubblicati in una data speifica:
+    articoli_del_giorno=Articolo.objects.filter(data=datetime.date(2023,1,1))
+
+    #9. Tutti gli articoli pubblicati in un intervallo di date:
+    articoli_periodo=Articolo.objects.filter(data__range=(datetime.date(2023,1,1), datetime.date(2024,1,1)))
+
+    #10. Gli articoli scritti da giornaisti nati prima del 2000:
+    giornalisti_nati=Giornalista.objects.filter(anno_di_nascita__lt=datetime.date(2000,1,1))
+    articoli_giornalisti=Articolo.objects.filter(giornalista__in=giornalisti_nati)
+
+    #11. Il giornalista più giovane:
+    giornalista_giovane=Giornalista.objects.order_by('-anno_di_nascita').first()
+
+    #12. Il giornalista più anziano:
+    giornalista_anziano=Giornalista.objects.order_by('anno_di_nascita').first()
+    
+    #13. Gli ultimi 5 articoli pubblicati:
+    ultimi=Articolo.objects.order_by('-data')[:5]
+
+    #14. Tutti gli articoli con un certo numero minimo di visualizzazioni:
+    articoli_minime_visual=Articolo.objects.filter(visualizzazzioni__gte=10)
+    
+    #15. Tutti gli articoli che contengono una certa parola all'inizio:
+    articoli_parola=Articolo.objects.filter(titolo__icontains='importante')
+
+    context={
+        'articoli_cognome':articoli_cognome,
+        'numero_totale_articoli':numero_totale_articoli,
+        'giornalista_1':giornalista_1,
+        'numero_articoli_giornalista_1':numero_articoli_giornalista_1,
+        'articoli_ordinati':articoli_ordinati,
+        'articoli_senza_visualizzazioni':articoli_senza_visualizzazioni,
+        'articolo_piu_visualizzato':articolo_piu_visualizzato,
+        'giornalisti_data':giornalisti_data,
+        'articoli_del_giorno':articoli_del_giorno,
+        'articoli_periodo':articoli_periodo,
+        'articoli_giornalisti':articoli_giornalisti,
+        'giornalista_giovane':giornalista_giovane,
+        'giornalista_anziano':giornalista_anziano,
+        'ultimi':ultimi,   
+        'articoli_minime_visual':articoli_minime_visual,
+        'articoli_parola':articoli_parola,
+    }
+    return render(request, 'query.html', context)
+
+    
